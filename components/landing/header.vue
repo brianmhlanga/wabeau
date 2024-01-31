0d47c05cbbad77e26846ae88bc10e4690b0c84de
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-01-31 16:40:34 +0200

    Updated header component

--- components/landing/header.vue
+++ components/landing/header.vue
@@ -1,13 +1,12 @@
 <template>
-    <div class="relative p-8 overflow-hidden bg-no-repeat bg-cover" style="background-image: url(&quot;/images/school.png&quot;);background-size: cover;height: 1000px;">
+    <div class="relative p-8 overflow-hidden bg-no-repeat bg-cover" style="background-image: url(&quot;/images/models.png&quot;);background-size: cover;height: 520px;">
         <div class="text-center myy-6 relative">
-            <div class="text-6xl font-bold mb-1 text-white">Ace Your Exams with</div>
-            <div class="text-6xl text-blue-400 font-bold mb-4">Mfundo!</div>
-            <p class="mt-0 mb-3 line-height-3 text-center mx-auto text-blue-100" style="max-width: 500px;">Independent Exam Creation, Answer Selection, Customizable Time Limits.</p>
+            <div class="text-6xl font-bold mb-1 text-white" ref="target"></div>
+            <img src="/images/logo.png" height="100" />
             <div class="flex align-items-center justify-content-center flex-wrap">
-            <button style="height: 75px;width: 220px;font-size: 22px;border-radius: 40px;" class="p-button p-component p-3 mt-3" type="button" aria-label="Notify Me" data-pc-name="button" data-pc-section="root" data-pd-ripple="true">
+            <button style="height: 55px;width: 190px;font-size: 19px;border-radius: 40px;" class="p-button p-component p-3 mt-3 ghd" type="button" aria-label="Notify Me" data-pc-name="button" data-pc-section="root" data-pd-ripple="true">
                 <!---->
-                <span class="p-button-label" data-pc-section="label">Explore Exams</span>
+                <span class="p-button-label" data-pc-section="label">Competitions</span>
                 <!---->
                 <span role="presentation" aria-hidden="true" data-p-ink="true" data-p-ink-active="false" class="p-ink" data-pc-name="ripple" data-pc-section="root"></span>
             </button>
@@ -15,12 +14,29 @@
         </div>
     </div>
 </template>
+<script lang="ts" setup>
+ import Typewriter from 'typewriter-effect/dist/core';
+
+const target = ref();
+
+onMounted(() => {
+    const typewriter = new Typewriter(target.value, {
+      delay: 75,
+      loop: true
+    });
+
+    typewriter
+        .typeString('Empower Glamour')
+        .pauseFor(1000)
+        .deleteAll(25)
+        .typeString('Elevate Grace')
+        .pauseFor(1000)
+        .deleteAll(25)
+        .start();
+});
+</script>
 <style>
-.text-center.myy-6.relative {
-    margin-top: 130px;
-    margin-bottom: 10px;
-    font-size: 0px;
-}
+
 .text-6xl {
     font-size: 6rem!important;
 }
@@ -29,5 +45,16 @@ p {
     font-weight: 400;
     line-height: 25px;
     margin: 0px;
+}
+.text-center.myy-6.relative {
+    margin-top: 30px;
+    background-color: #fcab2b36;
+    padding: 25px;
+    margin-bottom: 10px;
+    font-size: 0px;
+}
+button.p-button.p-component.p-3.mt-3.ghd {
+    background-color: #A5CB3A;
+    border-color: #A5CB3A;
 }
 </style>
