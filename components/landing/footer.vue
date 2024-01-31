38fae0da506d301ca4e0bd5fc1bb41186a6aecfd
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-01-31 16:40:20 +0200

    Added footer component

---
+++ components/landing/footer.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="block-content">
+  <div class="">
+    <div class="grid grid-nogutter surface-section px-4 py-4 md:px-6 lg:px-8 border-top-1 surface-border">
+      <div class="col-12 lg:col-6 lg:border-right-1 surface-border">
+        <img src="/images/logo.png" class="w-9rem mx-auto lg:mx-0" alt="Peak logo">
+        <span class="text-900 block mt-4 mr-3">Welcome to Wabeau, your ultimate runway to the world of glamour and style! Wabeau is more than just a platform; it's a stage where aspiring models showcase their unique charisma and where fashion enthusiasts become the judges. Dive into the pulse of the latest trends, discover breathtaking talent, and cast your votes to shape the next fashion icon</span>
+        <span class="text-500 block mt-4">© 2024, Wabeau. <a href="https://www.webdev.co.zw/">Powered by Webdev Group</a></span>
+      </div>
+      <div class="col-12 md:col-6 lg:col-3 mt-4 lg:mt-0 lg:pl-4 flex flex-column">
+        <span class="text-900 text-xl font-medium block">Company</span>
+        <ul class="list-none p-0">
+          <li>
+            <a tabindex="0" class="text-600 hover:text-900 transition-duration-150 cursor-pointer mt-3 block">About Peak</a>
+          </li>
+          <li>
+            <a tabindex="0" class="text-600 hover:text-900 transition-duration-150 cursor-pointer mt-3 block">Factories</a>
+          </li>
+          <li>
+            <a tabindex="0" class="text-600 hover:text-900 transition-duration-150 cursor-pointer mt-3 block">Careers</a>
+          </li>
+          <li>
+            <a tabindex="0" class="text-600 hover:text-900 transition-duration-150 cursor-pointer mt-3 block">Environmental Initiatives</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-12 lg:col-3 md:col-6 flex mt-4 lg:mt-0 lg:pl-4 flex-column">
+        <span class="text-900 text-xl font-medium block">Account</span>
+        <ul class="list-none p-0">
+          <li>
+            <a tabindex="0" class="text-600 hover:text-900 transition-duration-150 cursor-pointer mt-3 block">Manage Account</a>
+          </li>
+          <li>
+            <a tabindex="0" class="text-600 hover:text-900 transition-duration-150 cursor-pointer mt-3 block">Saved Items</a>
+          </li>
+          <li>
+            <a tabindex="0" class="text-600 hover:text-900 transition-duration-150 cursor-pointer mt-3 block">My Cart</a>
+          </li>
+          <li>
+            <a tabindex="0" class="text-600 hover:text-900 transition-duration-150 cursor-pointer mt-3 block">Wishlist</a>
+          </li>
+          <li>
+            <a tabindex="0" class="text-600 hover:text-900 transition-duration-150 cursor-pointer mt-3 block">Orders &amp; Returns</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="surface-900 py-6 lg:py-4 md:px-6 lg:px-8 flex flex-column lg:flex-row justify-content-between align-items-center">
+      <ul class="list-none p-0 mb-0 flex flex-column md:flex-row flex-order-1 lg:flex-order-0 mt-4 lg:mt-0">
+        <li class="mr-4 mt-3 lg:mt-0">
+          <a tabindex="0" class="cursor-pointer text-0">Data Privacy</a>
+        </li>
+        <li class="mr-4 mt-3 lg:mt-0">
+          <a tabindex="0" class="cursor-pointer text-0">Terms of Service</a>
+        </li>
+        <li class="mr-4 mt-3 lg:mt-0">
+          <a tabindex="0" class="cursor-pointer text-0">Legal Information</a>
+        </li>
+      </ul>
+      <div class="flex align-items-center flex-order-0 lg:flex-order-1">
+        <a tabindex="0" class="cursor-pointer mr-3 lg:mt-0 block">
+          <i class="pi pi-facebook surface-section p-1 text-sm border-circle text-900"></i>
+        </a>
+        <a tabindex="0" class="cursor-pointer mr-3 lg:mt-0 block">
+          <i class="pi pi-twitter surface-section p-1 text-sm border-circle text-900"></i>
+        </a>
+        <a tabindex="0" class="cursor-pointer mr-3 lg:mt-0 block">
+          <i class="pi pi-youtube surface-section p-1 text-sm border-circle text-900"></i>
+        </a>
+        <a tabindex="0" class="cursor-pointer lg:mt-0 block">
+          <i class="pi pi-google surface-section p-1 text-sm border-circle text-900"></i>
+        </a>
+      </div>
+    </div>
+  </div>
+  <!---->
+</div>
+</template>
