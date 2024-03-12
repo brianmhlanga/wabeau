ae5d364f2c1f2b80f50ad18de8f34f231d82bd21
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-03-12 16:14:32 +0200

    added users

---
+++ server/routes/backoffice/allusers.post.ts
@@ -0,0 +1,20 @@
+import { prisma } from "~~/prisma/db";
+
+export default defineEventHandler(async (event)=>{
+    const response: any = {};
+   
+    try {
+        const users = await prisma.user.findMany({
+          
+        });
+  
+        response['users'] =  users
+        response['success'] = true
+  
+      } catch (error: any) {
+        response['success'] = false
+        response['message'] = error.toString()
+      };
+   
+    return response;
+});
