08931f3a42db12700e2c8f2c9b276b945d15b341
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-03-12 16:10:58 +0200

    updated competition redirect

---
+++ pages/competitionRedirect-[competitionId]-[modelId].vue
@@ -0,0 +1,9 @@
+<template>
+
+</template>
+<script lang="ts" setup>
+const { params: { modelId, competitionId } }: any = useRoute();
+onMounted(() => {
+    navigateTo(`/shareLogin-${competitionId}-${modelId}`)
+})
+</script>
